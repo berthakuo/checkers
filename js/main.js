37bddc8a5d85a615e86e77adea409ca0e7c2f75f
berthakuo <berthakuo@gmail.com>
2015-06-28 20:01:31 -0400

    added logic to check if move is correct

--- js/main.js
+++ js/main.js
@@ -1,28 +1,117 @@
-function reset () {
-	$("[data-row='1']").append("<div class='piece piece--red'></div>");
-	$("[data-row='2']").append("<div class='piece piece--red'></div>");
-	$("[data-row='7']").append("<div class='piece piece--black'></div>");
-	$("[data-row='8']").append("<div class='piece piece--black'></div>");
-}
+//test
+// $("#square15").append("<div class='piece red'></div>");
 
+reset();
+
+// ************************************************************************
+// VARIABLES
+// ************************************************************************
+var selectedPiece = {};
+var selectedSquare = {};
+
+// ************************************************************************
+// EVENT HANDLERS
+// ************************************************************************
 $(".piece").click(selectPiece);
 $(".square").click(selectSquare);
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+        checkMove();
+    }
+});
 
-function Piece(piece, x, y) {
-	this.piece = piece;
+// ************************************************************************
+// FUNCTIONS
+// ************************************************************************
+
+function reset() {
+	//remove all leftover pieces from the board
+	$(".piece").remove();
+
+	//setup red pieces
+	$("[data-row='1']:nth-child(even)").append("<div class='piece red'></div>");
+	$("[data-row='2']:nth-child(odd)").append("<div class='piece red'></div>");
+	$("[data-row='3']:nth-child(even)").append("<div class='piece red'></div>");
+
+	//setup black pieces
+	$("[data-row='6']:nth-child(odd)").append("<div class='piece black'></div>");
+	$("[data-row='7']:nth-child(even)").append("<div class='piece black'></div>");
+	$("[data-row='8']:nth-child(odd)").append("<div class='piece black'></div>");
+}
+
+function Selection(holder, x, y, color) {
+	this.holder = holder;
 	this.x = x;
 	this.y = y;
+	this.color = color;
 }
 
 function selectPiece(event){
 	var piece = $(event.target);
 	var square = $(piece.parent(".square"));
-	var x = parseInt(square.attr("data-row"));
-	var y = parseInt(square.attr("data-column"));
-
-	return new Piece(piece, x, y);
+	var y = parseInt(square.attr("data-row"));
+	var x = parseInt(square.attr("data-column"));
+	var color = piece.hasClass("red")? 'red': 'black';
+	selectedPiece = new Selection(piece, x, y, color);
 }
 
+function selectSquare(event){
+	var square = $(event.target);
+	var y = parseInt(square.attr("data-row"));
+	var x = parseInt(square.attr("data-column"));
+	selectedSquare = new Selection (square, x, y)
+}
+
+function moveChecker(){
+	//add checker to selected square
+	//QUESTION - not sure why I dont need a remove function too
+	selectedSquare.holder.append(selectedPiece.holder);
+}
+
+function convertToID(x,y) {
+	number = (y-1)*8 + x;
+	return "square" + number;
+}
+
+function checkMove(){
+	if (selectedPiece.color == 'red')
+		if (selectedPiece.x + 1 == selectedSquare.x && selectedPiece.y + 1 == selectedSquare.y) {
+			moveChecker();
+		}
+		else if (selectedPiece.x - 1 == selectedSquare.x && selectedPiece.y + 1 == selectedSquare.y) {
+			moveChecker();
+		}
+		else if (selectedPiece.x + 2 == selectedSquare.x && selectedPiece.y + 2 == selectedSquare.y) {
+			moveChecker();
+			removeOpponent();
+		}
+		else if (selectedPiece.x - 2 == selectedSquare.x && selectedPiece.y + 2 == selectedSquare.y) {
+			moveChecker();
+			removeOpponent();
+		}
+		else {
+			console.log("wrong move buddy!")
+		}
+	else {
+		if (selectedPiece.x + 1 == selectedSquare.x && selectedPiece.y - 1 == selectedSquare.y) {
+			moveChecker();
+		}
+		else if (selectedPiece.x - 1 == selectedSquare.x && selectedPiece.y - 1 == selectedSquare.y) {
+			moveChecker();
+		}
+		else if (selectedPiece.x + 2 == selectedSquare.x && selectedPiece.y - 2 == selectedSquare.y) {
+			moveChecker();
+			removeOpponent();
+		}
+		else if (selectedPiece.x - 2 == selectedSquare.x && selectedPiece.y - 2 == selectedSquare.y) {
+			moveChecker();
+			removeOpponent();
+		}
+		else {
+			console.log("wrong move buddy!")
+		}
+	}
+}
 
 
 
